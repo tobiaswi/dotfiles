80e30fd09368a5ec5d08888e78ef3ff9625aa96d
Tobias Wischmeier <tobias.wischmeier@outlook.com>
2019-04-20 20:40:56 +0200

    removed obsolete entries and added gpg config

--- custom.zsh
+++ custom.zsh
@@ -3,7 +3,6 @@ alias g="$HOME/go"
 alias h="history"
 alias kx="kubectx"
 alias kn="kubens"
-alias pwsh=powershell
 alias zshconfig="vim ~/.zshrc"
 alias zshcustom="vim ~/.oh-my-zsh/custom/custom.zsh"
 
@@ -12,13 +11,13 @@ zshreload() {
 }
 
 export DEFAULT_USER="`whoami`"
-#export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 export PATH="$PATH:`yarn global bin`"
 export GPG_TTY=$(tty)
+export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+gpg-connect-agent updatestartuptty /bye
 
 source /opt/az/bin/az.completion.sh
 source <(kubectl completion zsh)
 source <(minikube completion zsh)
 source <(helm completion zsh)
-#complete -o nospace -C /opt/terraform/terraform terraform
